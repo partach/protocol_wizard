e04832ca9072f831bf69bf3bbf7f8f022773023f
partach <partach@gmail.com>
2026-01-04 22:38:06 +0100

    Update __init__.py

--- custom_components/protocol_wizard/protocols/snmp/__init__.py
+++ custom_components/protocol_wizard/protocols/snmp/__init__.py
@@ -2,3 +2,9 @@
 #-- protocol snmp init.py protocol wizard
 #------------------------------------------
 
+"""SNMP protocol plugin."""
+from .coordinator import SNMPCoordinator
+from .client import SNMPClient
+from .const import CONF_ENTITIES, SNMP_DATA_TYPES, oid_key
+
+__all__ = ["SNMPCoordinator", "SNMPClient", "CONF_ENTITIES", "SNMP_DATA_TYPES", "oid_key"]
