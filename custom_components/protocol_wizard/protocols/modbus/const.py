ddd2c7c7efa0c7a4d7ca5e5c81c294665fa19bd5
partach <partach@gmail.com>
2026-01-04 17:13:13 +0100

    Update const.py

--- custom_components/protocol_wizard/protocols/modbus/const.py
+++ custom_components/protocol_wizard/protocols/modbus/const.py
@@ -1,3 +1,20 @@
 #------------------------------------------
 #-- protocol modbus const.py protocol wizard
 #------------------------------------------
+"""Modbus-specific constants."""
+
+CONF_ENTITIES = "registers"
+
+TYPE_SIZES = {
+    "uint16": 1,
+    "int16": 1,
+    "uint32": 2,
+    "int32": 2,
+    "float32": 2,
+    "uint64": 4,
+    "int64": 4,
+}
+
+def reg_key(name: str) -> str:
+    """Generate consistent key from register name."""
+    return name.lower().strip().replace(" ", "_")
