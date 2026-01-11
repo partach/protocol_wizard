49a44c30879f0f2085b9613885ac044c12db8a43
partach <partach@gmail.com>
2026-01-11 18:46:43 +0100

    Create __init__.py

---
+++ custom_components/protocol_wizard/protocols/mqtt/__init__.py
@@ -0,0 +1,25 @@
+# custom_components/protocol_wizard/protocols/mqtt/__init__.py
+"""MQTT protocol implementation."""
+from .client import MQTTClient
+from .coordinator import MQTTCoordinator
+from .const import (
+    CONF_ENTITIES,
+    CONF_BROKER,
+    CONF_PORT,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    DEFAULT_PORT,
+    DATA_TYPES,
+)
+
+__all__ = [
+    "MQTTClient",
+    "MQTTCoordinator",
+    "CONF_ENTITIES",
+    "CONF_BROKER",
+    "CONF_PORT",
+    "CONF_USERNAME",
+    "CONF_PASSWORD",
+    "DEFAULT_PORT",
+    "DATA_TYPES",
+]
